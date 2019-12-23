eda18701348839c5c8e54295e96020a3b2b99366
georgeweigt <yo73137@gmail.com>
2019-12-23 14:57:45 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -1,6 +1,4 @@
-/* December 6, 2019
-
-License: FreeBSD
+/* December 22, 2019
 
 To build and run:
 
@@ -16,8 +14,34 @@ To run a script:
 A test script is available here:
 
 	www.eigenmath.org/selftest1
-*/
 
+
+BSD 2-Clause License
+
+Copyright (c) 2019, George Weigt
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -10419,6 +10443,11 @@ char *itab[] = {
 // muon production
 
 	"f(cos(x)^2*sin(x),-cos(x)^3/3)",
+
+// compton scattering
+
+	"f(sin(x)/(a+b*cos(x)),-log(a+b*cos(x))/b)",
+	"f(sin(x)/(a+b*(cos(x)-1)),log(a+b-b*cos(x))/b)",
 
 	NULL,
 };
